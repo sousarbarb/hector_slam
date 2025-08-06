db37d9350e0b4f44dee024b76ef9ca802659016b
Ricardo B. Sousa <sousa.ricardob@outlook.com>
2025-08-06 17:20:18 +0100

    minor fix

--- hector_mapping/src/HectorMappingROS1Offline.cpp
+++ hector_mapping/src/HectorMappingROS1Offline.cpp
@@ -606,7 +606,7 @@ void HectorMappingROS1Offline::restoreTerminal()
   }
 
   const int fd = fileno(stdin);
-  m_orig_flags_.c_lflag |= (ICANON);  // always restore canonical mode
+  orig_flags_.c_lflag |= (ICANON);  // always restore canonical mode
   tcsetattr(fd, TCSANOW, &orig_flags_);
 
   terminal_modified_ = false;
